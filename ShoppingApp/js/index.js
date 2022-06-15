703a96a46107d6955b46a57674b79926ac3947a9
Teja <tejaabp@gmail.com>
2022-06-15 16:33:52 +0530

    continue...

--- ShoppingApp/js/index.js
+++ ShoppingApp/js/index.js
@@ -11,3 +11,21 @@ const fetchProducts  =  async () => {
     productSpinner.style.display = 'none'
     data.forEach(product => createProductDiv(product))
 }       
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
