a76240596809c750e036a44542370e3e7994ced4
Teja <tejaabp@gmail.com>
2022-06-21 16:26:23 +0530

    bank interest management updated

--- BankInterestManagement/index.js
+++ BankInterestManagement/index.js
@@ -21,7 +21,7 @@ function calculateInterest(){
             temp *= (1 + 0.03)
             // temp = temp + temp * 0.03
         }
-        updatedValues[i].value = temp
+        updatedValues[i].value = Math.round(temp)
     }
 
 }
