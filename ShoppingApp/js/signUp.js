f2bcd58295c5c70ff7530c9490c6c1d13bdfa33d
Teja <tejaabp@gmail.com>
2022-06-27 12:08:37 +0530

    shoppinngg app signUp done

--- ShoppingApp/js/signUp.js
+++ ShoppingApp/js/signUp.js
@@ -1,8 +1,11 @@
 const DB_USERS = []
 
+let signUpNav = document.getElementById('sign-up-nav')
+let loginNav = document.getElementById('login-nav')
+
 const signUp = () => {
   let isValid = validate(true)
-    
+
 	if(isValid){
 		let firstNameFields = signUpForm.getElementsByClassName('form-fields')[0]
 		let lastNameFields = signUpForm.getElementsByClassName('form-fields')[1]
@@ -22,9 +25,19 @@ const signUp = () => {
 		}
 
 		DB_USERS.push(newUser)
-
+		console.log(DB_USERS[0].email)
 		reset()
 		$('#signUpModal').modal('hide')
+
+		let previousSignUpNav = signUpNav.innerHTML
+		let previosLoginNav =  loginNav.innerHTML
+
+		signUpNav.innerHTML	 = `<a class="nav-link"> Hi ${newUser.firstName}</a>`
+		loginNav.innerHTML	 = `<a class="nav-link" onclick='logout'> Logout</a>`
 		
+		loginNav.addEventListener('click', () => {
+			signUpNav.innerHTML	 = previousSignUpNav	
+			loginNav.innerHTML =  previosLoginNav
+		})
 	}
 }
--- ShoppingApp/js/validate.js
+++ ShoppingApp/js/validate.js
@@ -1,11 +1,16 @@
 let  signUpForm = document.getElementById('signUpForm')
-let globalFlag = false
+let signUpglobalFlag = false
+
+let loginForm = document.getElementById('loginForm')
+let loginglobalFlag = false
 
 let firstNameFields = signUpForm.getElementsByClassName('form-fields')[0]
 let lastNameFields = signUpForm.getElementsByClassName('form-fields')[1]
 let emailFields = signUpForm.getElementsByClassName('form-fields')[2]
 let passwordFields = signUpForm.getElementsByClassName('form-fields')[3]
 
+let loginEmailFields = loginForm.getElementsByClassName('form-fields')[0]
+let loginPasswordFields = loginForm.getElementsByClassName('form-fields')[1]
 
 let firstNameValid = firstNameFields.getElementsByClassName('valid-feedback')[0]
 let lastNameValid = lastNameFields.getElementsByClassName('valid-feedback')[0]
@@ -17,12 +22,18 @@ let lastNameInvalid = lastNameFields.getElementsByClassName('invalid-feedback')[
 let emailInvalid = emailFields.getElementsByClassName('invalid-feedback')[0]
 let passwordInvalid = passwordFields.getElementsByClassName('invalid-feedback')[0]
 
+let loginEmailValid = loginEmailFields.getElementsByClassName('valid-feedback')[0]
+let loginPasswordValid = loginPasswordFields.getElementsByClassName('valid-feedback')[0]
+let loginEmailInvalid = loginEmailFields.getElementsByClassName('invalid-feedback')[0]
+let loginPasswordInvalid = loginPasswordFields.getElementsByClassName('invalid-feedback')[0]
+
+
 const validate = (flag = false)  =>  {
     let isValid = true  
     if(flag)
-        globalFlag = flag
+    signUpglobalFlag = flag
 
-    if(globalFlag){
+    if(signUpglobalFlag){
         let firstName = firstNameFields.getElementsByClassName('form-control')[0].value
         let lastName = lastNameFields.getElementsByClassName('form-control')[0].value
         let email = emailFields.getElementsByClassName('form-control')[0].value
@@ -31,6 +42,7 @@ const validate = (flag = false)  =>  {
         if(!firstName){
             firstNameInvalid.style.display = 'block'
             firstNameValid.style.display = 'none'
+            isValid = false
         }
         else{
             firstNameValid.style.display = 'block'
@@ -39,6 +51,7 @@ const validate = (flag = false)  =>  {
         if(!lastName){
             lastNameInvalid.style.display = 'block'
             lastNameValid.style.display = 'none'
+            isValid = false
         }
         else{
             lastNameValid.style.display = 'block'
@@ -47,6 +60,7 @@ const validate = (flag = false)  =>  {
         if(!email || !email.includes('@') ||  !email.includes('.') || email.slice(email.lastIndexOf('.')+1).length < 2){
             emailInvalid.style.display = 'block'
             emailValid.style.display = 'none'
+            isValid = false
         }
         else{
             emailValid.style.display = 'block'
@@ -55,6 +69,7 @@ const validate = (flag = false)  =>  {
         if(!password || password.length < 8){
             passwordInvalid.style.display = 'block'
             passwordValid.style.display = 'none'
+            isValid = false
         }
         else{
             passwordValid.style.display = 'block'
@@ -63,12 +78,41 @@ const validate = (flag = false)  =>  {
     }
     return isValid
 }
+const loginValidate = (flag = false) => {
 
+    let isValid = true  
+    if(flag)
+    loginglobalFlag = flag
+
+    if(loginglobalFlag){
+        let email = loginEmailFields.getElementsByClassName('form-control')[0].value
+        let password = loginPasswordFields.getElementsByClassName('form-control')[0].value
+        if(!email || !email.includes('@') ||  !email.includes('.') || email.slice(email.lastIndexOf('.')+1).length < 2){
+            loginEmailInvalid.style.display = 'block'
+            loginEmailValid.style.display = 'none'
+            isValid = false
+        }
+        else{
+            loginEmailValid.style.display = 'block'
+            loginEmailInvalid.style.display = 'none'
+        }
+        if(!password || password.length < 8){
+            loginPasswordInvalid.style.display = 'block'
+            loginPasswordValid.style.display = 'none'
+            isValid = false
+        }
+        else{
+            loginPasswordValid.style.display = 'block'
+            loginPasswordInvalid.style.display = 'none'
+        }
+    }
+    return isValid
+}
 const reset = () => {
-    firstNameFields = signUpForm.getElementsByClassName('form-fields')[0]
-    lastNameFields = signUpForm.getElementsByClassName('form-fields')[1]
-    emailFields = signUpForm.getElementsByClassName('form-fields')[2]
-    passwordFields = signUpForm.getElementsByClassName('form-fields')[3]
+    firstNameFields.getElementsByClassName('form-control')[0].value  = ''
+    lastNameFields.getElementsByClassName('form-control')[0].value  = ''
+    emailFields.getElementsByClassName('form-control')[0].value  = ''
+    passwordFields.getElementsByClassName('form-control')[0].value  = ''
     
     firstNameValid.style.display = 'none'
     lastNameValid.style.display = 'none'
@@ -80,4 +124,18 @@ const reset = () => {
     passwordInvalid.style.display = 'none'
 
     globalFlag = false  
+}
+
+const loginReset = () => {
+   
+    loginEmailFields.getElementsByClassName('form-control')[0].value  = ''
+    loginPasswordFields.getElementsByClassName('form-control')[0].value  = ''
+       
+    loginEmailValid.style.display = 'none'
+    loginPasswordValid.style.display = 'none'
+   
+    loginEmailInvalid.style.display = 'none'
+    loginPasswordInvalid.style.display = 'none'
+
+    loginglobalFlag = false  
 }
