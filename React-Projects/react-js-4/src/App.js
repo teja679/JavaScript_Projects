401f1247d53da1002e0ca44c2876d51f7e5ba910
Teja <tejaabp@gmail.com>
2022-06-28 19:06:39 +0530

    component phases

---
+++ React-Projects/react-js-4/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <NavbarComponent />
+    </div>
+  );
+}
+
+export default App;
--- React-Projects/react-js3/src/App.js
+++ React-Projects/react-js3/src/App.js
@@ -1,11 +1,45 @@
-import './App.css';
+import React, { Component } from 'react'
+import ChildComponent from './components/ChildComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <h1> hello</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    console.log("Inside constructor")
+    this.state = {
+      count: 0,
+      show : true
+    }
+  }
+
+  componentDidUpdate() {
+    console.log("Inside componentDidUpdate")
+    console.log('----------------------UPDATING FINISHED------------------------')
+  }
+
+  componentDidMount() {
+    console.log("Inside componentDidMount")
+    console.log('----------------------MOUNTING FINISHED---------------------')
+  }
+  componentWillUnmount() {
+    console.log("Inside componentWillUnmount")
+    console.log('----------------------MOUNTING FINISHED---------------------')
+  }
+
+ 
+  render() {
+    console.log("Inside render")
+    return (
+      <>
+        <h1>App</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={() => 
+        this.setState((prevState) => ({count:prevState.count + 1}))}>Increment</button> <br /><br />
+        <h1>{this.state.show &&<ChildComponent />}</h1> <br /><br />
+        <button onClick={() => 
+        this.setState((prevState) => ({show:!prevState.show}))}>Show / Hide</button> <br /><br />
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
