d66977c75710ec499c13147d49576a08c636ac6a
Teja <tejaabp@gmail.com>
2022-06-30 10:39:26 +0530

    todo project completed

--- React_Projects/todolist/src/components/ToDo/ToDo.js
+++ React_Projects/todolist/src/components/ToDo/ToDo.js
@@ -3,6 +3,7 @@ import { Container, Form,Button,ListGroup, Col, InputGroup, Row} from 'react-boo
 import { FaEdit } from 'react-icons/fa';
 import { FiDelete } from 'react-icons/fi'
 import { MdOutlineBorderAll } from 'react-icons/md'
+import {AiOutlineCheckCircle } from 'react-icons/ai'
 import './ToDo.css'
 
 class ToDo extends Component{
@@ -10,11 +11,51 @@ class ToDo extends Component{
     super(props)
       this.state = {
         inputToDo : "",
-        toDoList : ['Revise Asynchronous JavaScript', 'Practive Array Method Questions', 'Revise React Components and Props', 'Revise React states']
+        toDoList : [],
+        isEditing : false,
+        editingIndex : ''
+        // ['Revise Asynchronous JavaScript', 'Practive Array Method Questions', 'Revise React Components and Props', 'Revise React states']
       }
   }
+
+  editToDo(inputindex) {
+    this.setState((prevState) => ({
+      inputToDo : prevState.toDoList[inputindex],
+      isEditing : true,
+      editingIndex : inputindex
+    }))
+  }
+
+  addOrUpdateToDo(){
+    const {inputToDo, isEditing, editingIndex} = this.state
+    if(inputToDo){
+      if(isEditing){
+        this.setState((prevState) => ({
+          toDoList : prevState.toDoList.map((todo, index) => {
+            if(index === editingIndex)
+              todo = inputToDo
+              return todo
+          }),
+          inputToDo : ''
+        }))
+      }
+      else{
+        this.setState((prevState) => ({
+          toDoList : [...prevState.toDoList, inputToDo],
+          inputToDo : ''
+        }))
+      }
+    }
+  }
+  deleteToDo(inputindex) {
+    const {toDoList} = this.state
+    this.setState((prevState) => ({
+      toDoList : prevState.toDoList.filter((todo, index) => index != inputindex)
+    }))
+  }
+ 
   render(){
-    const { toDoList, inputToDo } = this.state
+    const { toDoList, inputToDo, isEditing } = this.state
     return(
       <Container style={{margin:'20px auto'}}>
         <Col md={{span:4, offset:4}}>
@@ -26,21 +67,21 @@ class ToDo extends Component{
               value={inputToDo}
               onChange={(e) => this.setState ({inputToDo : e.target.value})} />
             <Button 
-            variant="outline-secondary" 
+            variant="outline-dark" 
             id="button-addon2"
-            onClick={() => this.setState((prevState) => ({
-              toDoList : [...prevState.toDoList, inputToDo]
-            }))}
-            ><MdOutlineBorderAll /></Button>
+            onClick={() => this.addOrUpdateToDo()}
+            >{isEditing ? <AiOutlineCheckCircle /> : <MdOutlineBorderAll />}</Button>
           </InputGroup>
           <ListGroup className="to-do-list">
-              {toDoList.map(toDo => (
+              {toDoList.map((toDo, index) => (
                 <ListGroup.Item size='sm'>
                 <Row>
                   <Col md={9}> {toDo} </Col>
                   <Col md={3} className="action-btns">
-                    <Button variant="warning" id="button-addon2" size='sm'><FaEdit /> </Button>
-                    <Button variant="danger" id="button-addon2" size='sm'><FiDelete /></Button>
+                    <Button variant="warning" id="button-addon2" size='sm'
+                      onClick={() => this.editToDo(index)}><FaEdit /> </Button>
+                    <Button variant="danger" id="button-addon2" size='sm' 
+                      onClick={() => this.deleteToDo(index)}><FiDelete /></Button>
                   </Col>
                 </Row>
               </ListGroup.Item>
