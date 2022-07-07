a04be6a1fb4d38967b960012eba688002ea621c1
Teja <tejaabp@gmail.com>
2022-07-07 12:49:26 +0530

    claculator js

--- JS_Projects/Calculator/index.js
+++ JS_Projects/Calculator/index.js
@@ -1,4 +1,4 @@
-var dispalyArea = document.getElementById('display')
+var displayArea = document.getElementById('display')
 
 function isOperator(buttonText){
     if(buttonText == '+' || buttonText == '-' || buttonText == '*' || buttonText == '/' || buttonText == '%')
@@ -7,79 +7,79 @@ function isOperator(buttonText){
         return false
 }
 function buttonClicked(buttonText){
-    if(dispalyArea.innerText.length >= 24)
+    if(displayArea.innerText.length >= 24)
         return
 
     if(buttonText === 'CLR'){
-        dispalyArea.innerText = '0';
+        displayArea.innerText = '0';
         return
     }
     if(buttonText === 'DEL'){
-        dispalyArea.innerText = dispalyArea.innerText.slice(0,-1);
+        displayArea.innerText = displayArea.innerText.slice(0,-1);
         return
     }
 
     if(buttonText === '='){
         try{
-            dispalyArea.innerText = eval(dispalyArea.innerText);
+            displayArea.innerText = eval(displayArea.innerText);
         }
         catch(error){
-            dispalyArea.innerText = 0;
+            displayArea.innerText = 0;
         }
         return
     }
-    if(dispalyArea.innerText === '0'){
+    if(displayArea.innerText === '0'){
         if(buttonText === '0' || buttonText === '00')
         return
 
         if(buttonText === '.' || isOperator(buttonText)){
-            dispalyArea.innerText = '0' + buttonText;
+            displayArea.innerText = '0' + buttonText;
             return
         }
 
-        dispalyArea.innerText = buttonText;
+        displayArea.innerText = buttonText;
     }
     else{
-        if(isOperator(dispalyArea.innerText[dispalyArea.innerText.length - 1]) && isOperator(buttonText)){
-            dispalyArea.innerText = dispalyArea.innerText.slice(0,-1) + buttonText;
+        if(isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText)){
+            displayArea.innerText = displayArea.innerText.slice(0,-1) + buttonText;
             return
         }
-        else if(buttonText === '.' && dispalyArea.innerText.includes('.')){
-            let x = dispalyArea.innerText
+        else if(buttonText === '.' && displayArea.innerText.includes('.')){
+            let x = displayArea.innerText
             if(x.lastIndexOf('.') < x.lastIndexOf('+') || x.lastIndexOf('.') < x.lastIndexOf('-') || x.lastIndexOf('.') < x.lastIndexOf('*') ||
             x.lastIndexOf('.') < x.lastIndexOf('/') || x.lastIndexOf('.') < x.lastIndexOf('%')){
-                dispalyArea.innerText = dispalyArea.innerText + buttonText;
+                displayArea.innerText = displayArea.innerText + buttonText;
             }
             return
         }
         else{
-            dispalyArea.innerText = dispalyArea.innerText + buttonText;
+            displayArea.innerText = displayArea.innerText + buttonText;
         }
     }
 }
 
 // function buttonClicked(buttonText){
 //     if(buttonText != 'DEL' && buttonText != 'CLR'){
-//         if(dispalyArea.innerText === '0'){
+//         if(displayArea.innerText === '0'){
 //             if(buttonText != '0' && buttonText != '00'){
 //                 if(buttonText == '.' || isOperator(buttonText)){
-//                     dispalyArea.innerText = dispalyArea.innerText + buttonText
+//                     displayArea.innerText = displayArea.innerText + buttonText
 //                 }else{
-//                     dispalyArea.innerText = buttonText
+//                     displayArea.innerText = buttonText
 //                 }
 //             }
 //         }else{
-//             if(isOperator(dispalyArea.innerText[dispalyArea.innerText.length-1]) && isOperator(buttonText)){
-//                 dispalyArea.innerText = dispalyArea.innerText.slice(0,-1) + buttonText  
+//             if(isOperator(displayArea.innerText[displayArea.innerText.length-1]) && isOperator(buttonText)){
+//                 displayArea.innerText = displayArea.innerText.slice(0,-1) + buttonText  
 //             }   else{
-//                 dispalyArea.innerText = dispalyArea.innerText + buttonText
+//                 displayArea.innerText = displayArea.innerText + buttonText
 //             }
 //         }
 //     } else{
 //         if(buttonText == 'CLR'){
-//             dispalyArea.innerText = '0'
+//             displayArea.innerText = '0'
 //         }else  if(buttonText == 'DEL'){
-//             dispalyArea.innerText = dispalyArea.innerText.slice(0,-1)
+//             displayArea.innerText = displayArea.innerText.slice(0,-1)
 //         }
 //     }
 // }
