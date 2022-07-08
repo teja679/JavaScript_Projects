aa8857f787ae9d3ef4d96fd1c8925a4456ab8517
Teja <tejaabp@gmail.com>
2022-07-08 13:11:35 +0530

    new calculator added

--- frontEndProjects/React_JS/calculator/src/components/Calculator/Calculator.js
+++ frontEndProjects/React_JS/calculator/src/components/Calculator/Calculator.js
@@ -1,53 +1,53 @@
-import React, { Component } from 'react'
 import './Calculator.css'
+import React, { useEffect, useState } from 'react'
 
-class Calculator extends Component {
-  constructor(props){
-    super(props)
-      this.state = {
-        display : '0',        // ['Revise Asynchronous JavaScript', 'Practive Array Method Questions', 'Revise React Components and Props', 'Revise React states']
-      }
-  }
-  buttonClicked(buttonText){
-    this.setState((prevState) => ({
-        display : 100
-    }))
-  }
-  render()  {
-    return (
-      <div>
-        <div className="App">
-          <div className="container">
-            <div id="display">{this.state.display}</div>
-            <div className="buttons">
-              <div className="button" onClick="buttonClicked('CLR')">C</div>
-              <div className="button" onClick="buttonClicked('DEL')">
-                DEL
-              </div>
-              <div className="button" onClick="buttonClicked('%')">%</div>
-              <div className="button" onClick="buttonClicked('/')">/</div>
-              <div className="button" onClick="buttonClicked('7')">7</div>
-              <div className="button" onClick="buttonClicked('8')">8</div>
-              <div className="button" onClick="buttonClicked('9')">9</div>
-              <div className="button" onClick="buttonClicked('*')">*</div>
-              <div className="button" onClick="buttonClicked('4')">4</div>
-              <div className="button" onClick="buttonClicked('5')">5</div>
-              <div className="button" onClick="buttonClicked('6')">6</div>
-              <div className="button" onClick="buttonClicked('-')">-</div>
-              <div className="button" onClick="buttonClicked('1')">1</div>
-              <div className="button" onClick="buttonClicked('2')">2</div>
-              <div className="button" onClick="buttonClicked('3')">3</div>
-              <div className="button" onClick="buttonClicked('+')">+</div>
-              <div className="button" onClick="buttonClicked('0')">0</div>
-              <div className="button" onClick="buttonClicked('00')">00</div>
-              <div className="button" onClick="buttonClicked('.')">.</div>
-              <div className="button" onClick="buttonClicked('=')">=</div>
+function Calculator() {
+const [ data, setData ] = useState(0)
+
+useEffect(() => {
+  
+})
+
+  return (
+    <div>
+      <div className="App">
+        <div className="container">
+          <div id="display">{data}</div>
+          <div className="buttons">
+            <div className="button" onClick={() => setData(0)}>C</div>
+            <div className="button" onClick={() => setData(data.slice(0,-1))}>
+              DEL
             </div>
+            <div className="button" onClick={() => setData(data + '%')}>%</div>
+            <div className="button" onClick={() => setData(data + '/')}>/</div>
+            <div className="button" onClick={() => setData(data == 0 ? '7' : data + '7')}>7</div>
+            <div className="button" onClick={() => setData(data == 0 ? '8' : data + '8')}>8</div>
+            <div className="button" onClick={() => setData(data == 0 ? '9' : data + '9')}>9</div>
+            <div className="button" onClick={() => setData(data + '*')}>*</div>
+            <div className="button" onClick={() => setData(data == 0 ? '4' : data + '4')}>4</div>
+            <div className="button" onClick={() => setData(data == 0 ? '5' : data + '5')}>5</div>
+            <div className="button" onClick={() => setData(data == 0 ? '6' : data + '6')}>6</div>
+            <div className="button" onClick={() => setData(data + '-')}>-</div>
+            <div className="button" 
+            onClick={() => {
+              setData(data === 0 ? '1' : data + '1')
+            }
+              }>1</div>
+            <div className="button" onClick={() => setData(data == 0 ? '2' : data + '2')}>2</div>
+            <div className="button" onClick={() => setData(data == 0 ? '3' : data + '3')}>3</div>
+            <div className="button" onClick={() => setData(data + '+')}>+</div>
+            <div className="button" onClick={() => setData(data == 0 ? '0' : data + '0')}>0</div>
+            <div className="button" onClick={() => setData(data == 0 ? '0' : data + '00')}>00</div>
+            <div className="button" onClick={() => setData(data + '.')}>.</div>
+            <div className="button" onClick={() => setData(data + '=')}>=</div>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Calculator
+
+/* <div className="button" onClick="buttonClicked('.')">.</div>
+            <div className="button" onClick="buttonClicked('=')">=</div>*/
---
+++ frontEndProjects/React_JS/photoapp/src/components/Photo.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Photo = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">
+        <img
+            src={photoLogo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+        />{' '}
+            Photo App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link as={NavLink} to='/'>Photos</Nav.Link>
+            <Nav.Link as={NavLink} to='/about'>About Us</Nav.Link>
+            <Nav.Link as={NavLink} to='/contact'>Contact Us</Nav.Link>
+            </Nav>
+            <Nav>
+            <Nav.Link>Sign Up</Nav.Link>
+            <Nav.Link>Login
+            </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Container>
+    </Navbar>
+  )
+}
+
+export default Photo
